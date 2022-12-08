e890deaf5a80bdce3cd39314dba6c373784387fd
Christopher <chrismbogo97@gmail.com>
2022-12-08 10:47:03 +0300

    Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,11 +22,31 @@ class ReviewsController < ApplicationController
     end
     
     # create Review
-    def create
-      review = Review.create!(review_params)
-      render json: review,  status: 201
+    # def create
+    #   review = Review.create!(review_params)
+    #   render json: review,  status: 201
+    # end
+  
+    def new
+      if logged_in? 
+        if @beer = Beer.find_by_id(params[:beer_id])
+          @review = @beer.reviews.build
+        else
+          @review = Review.new 
+        end
+      else
+        redirect_if_not_logged_in
+      end
     end
   
+    def create  
+      @review = current_user.reviews.build(review_params)
+      if @review.save
+        redirect_to review_path(@review)
+      else 
+        render :new 
+      end 
+    end 
     private 
   
     def find_review
